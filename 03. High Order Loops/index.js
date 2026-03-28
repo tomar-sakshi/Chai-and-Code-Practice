1ff4355d0fea80e0ecaacd40345d5c2a53c61933
ch-sakshi <ch.sakshi005@gmail.com>
2026-03-28 21:40:10 +0530

    add: High Order Loops

---
+++ 03. High Order Loops/index.js
@@ -0,0 +1,33 @@
+// for of
+
+// const arr = [3, 5, 8, 9, 4, 5];
+
+// for (const num of arr) {
+//   console.log(num);
+// }
+
+// Strings
+
+// const greetings = "Hello World!";
+// for (const greet of greetings) {
+//   console.log(greet);
+// }
+
+// Maps
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("Fr", "France");
+// map.set("Can", "Canada");
+// map.set("Jp", "Japan");
+
+// console.log(map);
+
+
+// for (const [key, value] of map) {
+// console.log(key, ':-', value); 
+// }
+
+const myObject = {
+    red: 'apple',
+}
