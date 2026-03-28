1538a9ccadbc5ae919e20965f8822a5ac16e2ec5
ch-sakshi <ch.sakshi005@gmail.com>
2026-03-28 21:39:47 +0530

    add: While Loop

---
+++ 02. While Loop/index.js
@@ -0,0 +1,34 @@
+// for (let i = 0; i < 10; i++) {
+//   if (i == 6) {
+//     console.log("Stop the code");
+//     break;
+//   } else if (i == 4 || i == 5) {
+//     console.log(`Skipping value  ${i}`);
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// while
+// let i = 0
+// while (i <= 15) {
+//     console.log(i);
+//    i= i+4;
+// }
+
+// let arr = ["Superman", "Batman", "Spiderman"]
+
+// let i = 0
+
+// while (i < arr.length){
+//     console.log(arr[i]);
+//     i = i+1;
+// }
+
+// Do While Loop
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
