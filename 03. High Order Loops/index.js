f205a6c2d11ebcfa03330e29f51ca945140a73be
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-01 11:36:39 +0530

    add: index.js

--- 03. High Order Loops/index.js
+++ 03. High Order Loops/index.js
@@ -23,11 +23,86 @@
 
 // console.log(map);
 
-
 // for (const [key, value] of map) {
-// console.log(key, ':-', value); 
+// console.log(key, ':-', value);
 // }
 
-const myObject = {
-    red: 'apple',
-}
+// const myObject = { // objects pr for of loops in not working
+//   red: "apple",
+//   green: "grapes",
+// };
+
+// for (const [key, value] of myObject) {
+//   console.log(key, ":-", value);
+// }
+
+// Objects
+
+// const myObject = {
+//   js: "javascript",
+//   cpp: "C++",
+//   rb: "ruby",
+//   swift: "swift by apple",
+// };
+// // for in loop
+// for (const key in myObject) {
+//   console.log(myObject[key]);
+// }
+
+// const programming = ['html', 'css', 'js', 'react', 'node']
+
+// for (const key in programming){
+//     console.log(programming[key]);
+// }
+
+//Maps
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("Fr", "France");
+// map.set("Can", "Canada");
+// map.set("Jp", "Japan");
+
+// for(const key in map){ // JavaScript mein Map objects par for...in loop kaam nahi karta kyunki for...in loop sirf enumeratable properties ko iterate karne ke liye bana hai, jabki Map keys/values ko store karta hai.
+//     console.log(key);
+// }
+
+const programming = ["html", "css", "js", "react", "node"];
+
+// programming.forEach(function(val){
+//     console.log(val);
+// })
+
+//Arrow Function
+// programming.forEach((value) => {
+//   console.log(value);
+// });
+
+// function printMe(value) {
+//   console.log(value);
+// }
+
+// programming.forEach(printMe);
+
+// programming.forEach((value, index, arr)=>{
+// console.log(value, index, arr);
+// })
+
+// const mycoding = [
+//   {
+//     languageName: "javascript",
+//     languageFileName: "js",
+//   },
+//   {
+//     languageName: "java",
+//     languageFileName: "java",
+//   },
+//   {
+//     languageName: "python",
+//     languageFileName: "py",
+//   },
+// ];
+
+// mycoding.forEach((item) => {
+//   console.log(item.languageName);
+// });
+
