9336096a67befd187350d764a5741548147efdd4
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-01 11:38:56 +0530

    add: scope.js

---
+++ 05. Function/scope.js
@@ -0,0 +1,51 @@
+// let a = 10;
+// const b = 20;
+// var c = 40;
+
+// var c = 400;
+// let a = 200;
+
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   console.log ( "Inner: ", a);
+//   var c = 30; // it is globe scope
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//   const username = "sakshi";
+
+//   function two() {
+//     const website = "youtube";
+//     console.log(username);
+//   }
+// //   console.log(website);
+//   two();
+// }
+// one();
+
+// if (true) {
+//   const username = "sakshi";
+//   if (username === "sakshi") {
+//     const website = "Instagram";
+//     console.log(username + website);
+//   }
+//   console.log(website);
+// }
+// console.log(username);
+
+// console.log(addone(5));// 6
+// function addone(num) {
+//   return num + 1; // in this we are only declaration function
+// }
+
+// const addTwo = function (num) {
+//   return num + 2; // in this we are declaration function and hold variables
+// };
+
+// addTwo(5);
+
