93f73929ec79265f9781c7e787e2c693dec402a6
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-01 11:37:56 +0530

    add: index2.js

---
+++ 05. Function/index2.js
@@ -0,0 +1,42 @@
+// const user = {
+//   username: "sakshi",
+//   price: 999,
+
+//   welcomeMessage: function () {
+//     console.log(`${this.username}, welcome to website`); // this means current context
+//     console.log(this);
+//   },
+// };
+
+// user.welcomeMessage();
+// user.username = "suryanshu";
+// user.welcomeMessage();
+
+// console.log(this);
+
+// const chai = () => {
+//   let username = "Sakshi";
+//   console.log(this.username);
+// };
+// chai();
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => ({username : "sakshi"})
+// console.log(addTwo(8, 6));
+
+// IIFE
+
+// (function chai() {
+//   console.log("DB CONNECTED");
+// })();
+
+// ((name) => {
+//   console.log("Db Connected Two" + name);
+// })("hitesh");
