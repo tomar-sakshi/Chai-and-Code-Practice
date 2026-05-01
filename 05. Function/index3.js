d5901975fefcc7b43d27b15eded75beddc9b09b5
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-01 11:38:09 +0530

    add: index3.js

---
+++ 05. Function/index3.js
@@ -0,0 +1,21 @@
+// const num1 = 30;
+// const num2 = 40;
+
+// function sum(num1,num2){
+//     const result = num1 + num2;
+//     return result;
+// }
+// console.log(sum(10,20))
+// console.log(sum(50,60))
+// console.log(sum(num1, num2));
+
+
+
+// function sub(num1, num2) {
+//   const result = num1 - num2;
+//   console.log(result);
+// }
+// sub(50, 60);
+// sub(-10, -20);
+// sub(15, -35);
+
