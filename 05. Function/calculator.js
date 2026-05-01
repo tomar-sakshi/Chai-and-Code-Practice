115770280ff9adbdac9e438c7e84613cea2326af
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-01 11:37:07 +0530

    add: index.js

---
+++ 05. Function/calculator.js
@@ -0,0 +1,201 @@
+// function calculator(num1, num2, action) {
+//   if (action == "sum") {
+//     const result = num1 + num2;
+//     return result;
+//   } else if (action == "sub") {
+//     const result = num1 - num2;
+//     return result;
+//   } else if (action == "mul") {
+//     const result = num1 * num2;
+//     return result;
+//   } else if (action == "div") {
+//     const result = num1 / num2;
+//     return result;
+//   } else if (action == "mod") {
+//     const result = num1 % num2;
+//     return result;
+//   } else if (action == "pow") {
+//     const result = num1 ** num2;
+//     return result;
+//   } else {
+//     return "wrong action";
+//   }
+// }
+
+// console.log(calculator(10, 20, "sum"));
+// console.log(calculator(40, 30, "sub"));
+// console.log(calculator(20, 2, "mul"));
+// console.log(calculator(40, 2, "div"));
+// console.log(calculator(40, 2, "mod"));
+// console.log(calculator(3, 2, "pow"));
+
+//*******************Practice question***************************//
+
+//Question 1. Check Positive or Negative. Take a number and print "Positive" if it’s greater than 0, otherwise "Negative".
+
+// Awswer-
+// const num = 4;
+// if (num > 0) {
+//   console.log("Postive");
+// } else {
+//   console.log("Negative");
+// }
+
+//Question 2- Even or Odd .Check if a number is even or odd.
+
+// Answer-
+// const num = 13;
+
+// if (num % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// Question 3 - Pass or Fail. Take marks as input. If marks ≥ 40 → "Pass", else "Fail".
+
+// Answer-
+// const marks = 60;
+// if (marks >= 40) {
+//   console.log("Pass");
+// } else {
+//   console.log("Fail");
+// }
+
+//Question 4- Voting Eligibility. Check if age is ≥ 18 → "Eligible to vote", else "Not eligible".
+
+// Answer-
+// const age = 28;
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+//Question 5-Greater Number. Compare two numbers and print the greater one.
+
+//Awswer-
+// const num1 = 20;
+// const num2 = 30;
+
+// if (num1 > num2) {
+//   console.log("Num1 is greater");
+// } else {
+//   console.log("Num2 is greater");
+// }
+
+//Question 6- Check Divisibility. Check if a number is divisible by 5.
+
+//Awswer-
+// const num = 34;
+// if (num % 5 === 0) {
+//   console.log("Divisiblity");
+// } else {
+//   console.log("Not Divisiblity");
+// }
+
+// Question 7- Temperature Check. If temperature > 30 → "Hot", else "Normal".
+
+//Answer-
+// const temperature = 35;
+// if (temperature > 30) {
+//   console.log("Hot");
+// } else {
+//   console.log("Normal");
+// }
+
+//Question 8- Simple Login Check. If username is "admin" and password is "1234", print "Login Successful", else "Invalid Credentials".
+
+// Awswer-
+// const username = "admin";
+// const password = "1234";
+// if (username === "admin" && password === "1234") {
+//   console.log("Login SuccessFul");
+// } else {
+//   console.log("Invalid Credentials");
+// }
+
+//Question 9-Number Comparison. Check if a number is greater than, less than, or equal to 10.
+
+// //Answer-
+// const num = 20;
+// if (num > 10) {
+//   console.log("Greater than 10");
+// } else if (num < 10) {
+//   console.log("Smaller than 10");
+// } else {
+//   console.log("Equal to 10");
+// }
+
+//Question 10 - Grade System (Basic) If marks ≥ 90 → "A", Else if marks ≥ 75 → "B", Else → "C".
+// Answer -
+// let marks = 90;
+// if (marks >= 90) {
+//   console.log("A Grade");
+// } else if (marks >= 75) {
+//   console.log("B Grade");
+// } else {
+//   console.log("C Grade");
+// }
+
+// Question 11- Check Zero or Not.Take a number and print "Zero" if it is 0, otherwise "Not Zero".
+
+// Answer 11-
+// const num = 0;
+// if (num == 0) {
+//   console.log("Zero");
+// } else {
+//   console.log("Not Zero");
+// }
+
+// Question 12 -Check Largest of Three Numbers (basic logic).Take 3 numbers and print the largest one.
+
+// Answer-
+// const num1 = 20;
+// const num2 = 30;
+// const num3 = 10;
+
+// if (num1 >= num2 && num1 >= num3) {
+//   console.log("Largest is: , num1");
+// } else if (num2 >= num1 && num2 >= num3) {
+//   console.log("Largest is ; num2");
+// } else {
+//   console.log("Largest is ; num3");
+// }
+
+//Question 13- Check Multiple of 3.If a number is divisible by 3 → print "Multiple of 3", else "Not a multiple".
+
+//Answer-
+// const num = 56;
+// if (num % 3 === 0) {
+//   console.log("Multiply by 3");
+// } else {
+//   console.log("Not a multiply");
+// }
+
+// Question 14- Simple Discount. If price > 1000 → apply 10% discount, else no discount.
+
+//Answer-
+// const price = 2999;
+// if (price > 1000) {
+//   console.log("Apply 10% Discount");
+// } else {
+//   console.log("No Discount");
+// }
+
+// Question 15- Check Character Type.Take a character and check if it is a vowel (a, e, i, o, u) or not.
+
+//Answer-
+// const char = "o";
+// if (
+//   char === "a" ||
+//   char === "e" ||
+//   char === "i" ||
+//   char === "o" ||
+//   char === "u"
+// ) {
+//   console.log("Vowel");
+// } else {
+//   console.log("Not Vowel");
+// }
+
