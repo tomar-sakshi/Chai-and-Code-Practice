dfc81bfd34bf28676f81913f45d5312561f76f7e
ch-sakshi <ch.sakshi005@gmail.com>
2026-05-08 16:18:08 +0530

    add: index2.js

--- 04. Filter Map and Reduce/index2.js
+++ 04. Filter Map and Reduce/index2.js
@@ -76,6 +76,22 @@
 // const negativeNumbers = numbers.filter((number) => number <= 0);
 // console.log(negativeNumbers);
 
+//Q11- Get all strings longer than 5 characters.
+//  Ans-
+// const words = [
+//   "Tanu",
+//   "Sakshi",
+//   "Kajal",
+//   "Shagun",
+//   "Aditi",
+//   "Saloni",
+//   "Priyanshi",
+// ];
+// const longWords = words.filter((word) => {
+//   return word.length > 5;
+// });
+// console.log(longWords);
+
 //Q12- Filter users with age greater than 18.
 // Ans-
 // const users = [
@@ -88,6 +104,76 @@
 // console.log(output);
 
 //Q13-Get only odd numbers from an array.
+// Ans-
+// const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const oddNumber = numbers.filter((number) => number % 2 !== 0);
+// console.log(oddNumber);
 
+//Q14- Remove all falsy values (false, 0, "", null, undefined)
+// Ans-
+// const arr = [0, "hello", false, "", 25, null];
+// const truthyValues = arr.filter(Boolean);
+// console.log(truthyValues);
 
+// Reduce
+// Q15- Find the sum of all numbers in an array
+// //Ans-
+// let arr = [2, 3, 6, 8, 9, 10];
+// function findSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+// console.log(findSum(arr));
 
+// const output = arr.reduce(function (acc, curr) {
+//   acc = acc + curr;
+//   return acc;
+// }, 0);
+// console.log(output);
+
+//Q16- Find the product of all numbers.
+// Ans-
+// let arr = [2, 3, 4, 5];
+// const output = arr.reduce(function (acc, curr) {
+//   acc = acc * curr;
+//   return acc;
+// });
+// console.log(output);
+
+//Q17-Find the maximum number in an array.
+// Ans-
+// const arr = [1, 3, 6, 9, 5, 4];
+// const output = arr.reduce(function (max, curr) {
+//   if (curr > max) {
+//     max = curr;
+//   }
+//   return max;
+// }, 0);
+// console.log(output);
+
+//Q18-Count how many elements are in an array (without using .length).
+// Ans-
+// const arr = [10, 20, 30, 40, 50];
+// const count = arr.reduce((total) => {
+//   return total + 1;
+// }, 0);
+// console.log(count);
+
+//Q19- Count how many times each element appears (frequency counter).
+//Ans-
+// const arr = ["a", "b", "a", "c", "b", "a"];
+
+//Q20- Find the total price from an array of objects {price: number}.
+//Ans-
+// const items = [
+//   { item: "pen", price: 20 },
+//   { item: "book", price: 100 },
+// ];
+// const totalPrice = items.reduce((total, currentItem) => {
+//   return total + currentItem.price;
+// }, 0);
+
+// console.log(totalPrice);
